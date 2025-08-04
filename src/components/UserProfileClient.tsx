57d00f283fc031817889b41285b2700cb7fad364
01akhil <bhadula245@gmail.com>
2025-08-04 18:18:14 +0530

    minor changes

--- src/components/UserProfileClient.tsx
+++ src/components/UserProfileClient.tsx
@@ -457,7 +457,7 @@ export default function UserProfileClient({ user }: Props) {
         return;
       }
 
-      const res = await fetch(`/api/user/${user._id}`, {
+      const res = await fetch(`/api/unfollow/${user._id}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
